30d0a940995fa1c545b0e7e57a740c745c98cd10
haavarhu <haavarhu@stud.ntnu.no>
2022-03-01 11:11:01 +0100

    Save data to mongodb database hosted on VM

--- transactions.py
+++ transactions.py
@@ -1,5 +1,6 @@
 import json
 import graphsense
+import pymongo
 from graphsense.api import addresses_api, entities_api, blocks_api, bulk_api
 import matplotlib.pyplot as plt
 from statistics import mode
@@ -7,10 +8,16 @@ from datetime import datetime
 from tqdm import tqdm
 from time import sleep, time
 
-transactionsFile = open("transactions.json", "a+")
+client = pymongo.MongoClient(
+    "mongodb://havardhuns:pwd@10.212.136.61/master?retryWrites=true&w=majority")
+db = client["master"]
+transactions_collection = db["transactions"]
+
+# empty collection
+transactions_collection.delete_many({})
 
 api_key = "i/cM9eSFHOvISa17naCYeo/g6qFCweoN"
-configuration = graphsense.Configuration(host = "https://api.graphsense.info")
+configuration = graphsense.Configuration(host="https://api.graphsense.info")
 configuration.api_key["api_key"] = api_key
 
 api_client = graphsense.ApiClient(configuration)
@@ -31,13 +38,14 @@ buffer = 5*60
 block_transactions = blocks_api.list_block_txs(currency, block_height)
 
 addresses = []
-transactions = []
 
 for transaction in block_transactions:
     for inp in transaction.inputs.value:
-        len(inp.address) > 0 and inp.address[0] not in addresses and addresses.append(inp.address[0])
+        len(inp.address) > 0 and inp.address[0] not in addresses and addresses.append(
+            inp.address[0])
     for outp in transaction.outputs.value:
-        len(outp.address) > 0 and outp.address[0] not in addresses and addresses.append(outp.address[0])
+        len(outp.address) > 0 and outp.address[0] not in addresses and addresses.append(
+            outp.address[0])
 
 print("Retreived", len(addresses), "from block with height", block_height)
 print("Getting all transactions addresses have been involved in")
@@ -61,10 +69,9 @@ for address in tqdm(addresses):
             calls = 0
 
     address_transactions = addresses_api.list_address_txs('btc', address)
-    address_transactions_hashes = [address_transaction.tx_hash for address_transaction in address_transactions.address_txs]
-    body = {"tx_hash": address_transactions_hashes }
+    address_transactions_hashes = [
+        address_transaction.tx_hash for address_transaction in address_transactions.address_txs]
+    body = {"tx_hash": address_transactions_hashes}
 
     api_response = bulk_call_api.bulk_json('btc', 'get_tx', 1, body)
-    transactions += api_response
-    with open("transactions.json", "w") as outfile:
-        outfile.write(json.dumps(transactions))
+    transactions_collection.insert_many(api_response)
