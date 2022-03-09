eed924b46960b2b67058c7a1cf91b9650baf801d
haavarhu <haavarhu@stud.ntnu.no>
2022-03-09 13:05:48 +0100

    Get transactions for an address, add to database, create graph for these transactions

--- testdb.py
+++ testdb.py
@@ -1,6 +1,5 @@
 import pymongo
 
-
 #connectURI = "mongodb://havardhuns:pwd@10.212.136.61/master?retryWrites=true&w=majority"
 connectURI = "mongodb://havardhuns:pwd@localhost/master?retryWrites=true&w=majority"
 
@@ -10,4 +9,4 @@ test_collection = db["test"]
 
 test_collection.insert_one({"test": "Funker"})
 
-print(test_collection.find_one_and_delete({"test":{'$exists': True}})['test'])
+print(test_collection.find_one_and_delete({"test": {'$exists': True}})['test'])
