cd34a68375b265250041984992935d56a6bcdeb3
haavarhu <haavarhu@stud.ntnu.no>
2022-03-21 11:28:55 +0100

    Create graphs for visualizing number of inputs in Transactions

--- multipleInputsOccurrencesGraphs.py
+++ multipleInputsOccurrencesGraphs.py
@@ -26,9 +26,9 @@ addresses_collection = db[f'nodes-{address}']
 transactions = collection.find({"coinbase": False})
 
 occurences_1_2_3plus = [0] * 3
-occurrences_3_to_19 = [0] * 17
-occurrences_20_to_99 = [0] * 80
-occurrences_100_to_max = [0]
+occurrences_3_to_20 = [0] * 18
+occurrences_21_to_210 = [0] * 190
+occurrences_211_to_max = [0]
 
 
 for transaction in tqdm(transactions):
@@ -39,33 +39,33 @@ for transaction in tqdm(transactions):
         occurences_1_2_3plus[1] += 1
     elif number_of_inputs > 2:
         occurences_1_2_3plus[2] += 1
-        if number_of_inputs < 20:
-            occurrences_3_to_19[number_of_inputs - 3] += 1
-        elif number_of_inputs < 100:
-            occurrences_20_to_99[number_of_inputs - 20] += 1
+        if number_of_inputs <= 20:
+            occurrences_3_to_20[number_of_inputs - 3] += 1
+        elif number_of_inputs <= 210:
+            occurrences_21_to_210[number_of_inputs - 21] += 1
         else:
-            if len(occurrences_100_to_max) < number_of_inputs - 99:
-                occurrences_100_to_max.extend(
-                    [0] * (number_of_inputs - 99 - len(occurrences_100_to_max)))
-            occurrences_100_to_max[number_of_inputs - 100] += 1
+            if len(occurrences_211_to_max) < number_of_inputs - 210:
+                occurrences_211_to_max.extend(
+                    [0] * (number_of_inputs - 210 - len(occurrences_211_to_max)))
+            occurrences_211_to_max[number_of_inputs - 211] += 1
 
 plt.bar(["1", "2", "3+"], occurences_1_2_3plus)
 plt.title("Number of inputs in transactions")
 plt.savefig('graphs/occurences_1_2_3plus.png')
 plt.close()
 
-plt.plot(range(3, 20), occurrences_3_to_19)
-plt.xticks(range(3, 20))
-plt.title("Transactions with 3 to 19 inputs")
-plt.savefig('graphs/occurrences_3_to_19.png')
+plt.plot(range(3, 21), occurrences_3_to_20)
+plt.xticks(range(3, 21))
+plt.title("Transactions with 3 to 20 inputs")
+plt.savefig('graphs/occurrences_3_to_20.png')
 plt.close()
 
-plt.plot(range(20, 100), occurrences_20_to_99)
-plt.title("Transactions with 20 to 99 inputs")
-plt.savefig('graphs/occurrences_20_to_99.png')
+plt.plot(range(21, 211), occurrences_21_to_210)
+plt.title("Transactions with 21 to 210 inputs")
+plt.savefig('graphs/occurrences_21_to_210.png')
 plt.close()
 
-plt.plot(range(100, len(occurrences_100_to_max) + 100), occurrences_100_to_max)
-plt.title("Transactions with 100 inputs or more")
-plt.savefig('graphs/occurrences_100_to_max.png')
+plt.plot(range(211, len(occurrences_211_to_max) + 211), occurrences_211_to_max)
+plt.title("Transactions with more than 210 inputs")
+plt.savefig('graphs/occurrences_211_to_max.png')
 plt.close()
