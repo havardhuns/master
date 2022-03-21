7d8f7cc5cfe6155eb574ee01c64f4f03b2e2fc25
haavarhu <haavarhu@stud.ntnu.no>
2022-03-21 11:28:55 +0100

    Create graphs for visualizing number of inputs in Transactions

---
+++ multipleInputsOccurrencesGraphs.py
@@ -0,0 +1,71 @@
+import networkx as nx
+import pymongo
+from tqdm import tqdm
+import os
+from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+
+
+'''
+Creates graphs visualizing how many inputs are in transactions
+'''
+
+load_dotenv('.env')
+
+address = "3NWBZKC9UZ6fYRDMwLDAM6hoD1mkT5WgAS"
+level = 2
+G = nx.DiGraph()
+
+connectURI = os.environ["connectURI"]
+client = pymongo.MongoClient(connectURI)
+db = client["master"]
+collection = db['transactions']
+addresses = db['addresses']
+addresses_collection = db[f'nodes-{address}']
+
+transactions = collection.find({"coinbase": False})
+
+occurences_1_2_3plus = [0] * 3
+occurrences_3_to_20 = [0] * 18
+occurrences_21_to_210 = [0] * 190
+occurrences_211_to_max = [0]
+
+
+for transaction in tqdm(transactions):
+    number_of_inputs = len(transaction["inputs"])
+    if number_of_inputs == 1:
+        occurences_1_2_3plus[0] += 1
+    elif number_of_inputs == 2:
+        occurences_1_2_3plus[1] += 1
+    elif number_of_inputs > 2:
+        occurences_1_2_3plus[2] += 1
+        if number_of_inputs <= 20:
+            occurrences_3_to_20[number_of_inputs - 3] += 1
+        elif number_of_inputs <= 210:
+            occurrences_21_to_210[number_of_inputs - 21] += 1
+        else:
+            if len(occurrences_211_to_max) < number_of_inputs - 210:
+                occurrences_211_to_max.extend(
+                    [0] * (number_of_inputs - 210 - len(occurrences_211_to_max)))
+            occurrences_211_to_max[number_of_inputs - 211] += 1
+
+plt.bar(["1", "2", "3+"], occurences_1_2_3plus)
+plt.title("Number of inputs in transactions")
+plt.savefig('graphs/occurences_1_2_3plus.png')
+plt.close()
+
+plt.plot(range(3, 21), occurrences_3_to_20)
+plt.xticks(range(3, 21))
+plt.title("Transactions with 3 to 20 inputs")
+plt.savefig('graphs/occurrences_3_to_20.png')
+plt.close()
+
+plt.plot(range(21, 211), occurrences_21_to_210)
+plt.title("Transactions with 21 to 210 inputs")
+plt.savefig('graphs/occurrences_21_to_210.png')
+plt.close()
+
+plt.plot(range(211, len(occurrences_211_to_max) + 211), occurrences_211_to_max)
+plt.title("Transactions with more than 210 inputs")
+plt.savefig('graphs/occurrences_211_to_max.png')
+plt.close()
