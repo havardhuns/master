6aef824bc14fed511d0478c63908cb2eaf302407
Håvard Hunshamar <havardhunshamar@vpn-10-50-228-134.vpn-a.ntnu.no>
2022-04-07 18:19:40 +0200

    clean up files + small improvements

--- checkEntitites.py
+++ checkEntitites.py
@@ -35,43 +35,66 @@ def get_entity_from_inputs(inputs):
 
 def get_addresses_from_entity(entity):
     addresses = []
-    response = entities_api.list_entity_addresses('btc', entity, pagesize=1000)
+    response = entities_api.list_entity_addresses('btc', entity, pagesize=5000)
     addresses.extend(response.addresses)
     while 'next_page' in response:
-        sleep(1)
-        response = entities_api.list_entity_addresses('btc', entity, page=response['next_page'], pagesize=1000)
+        sleep(10)
+        response = entities_api.list_entity_addresses('btc', entity, page=response['next_page'], pagesize=5000)
         addresses.extend(response.addresses)
     return [address["address"] for address in addresses]
+
+def delete_entity(entity, tx_hash):
+    if len(entity["tx_hash"]) > 1:
+        aggregated_inputs.update_one({
+                '_id': entity['_id']
+            }, 
+                {'$pull': {'tx_hash': tx_hash}
+            }, upsert=False)
+    else:
+        aggregated_inputs.delete_one({'_id': entity['_id']})
+        while 'next_page' in entity:
+            next_entity = aggregated_inputs.find_one({'_id': entity['next_page']})
+            aggregated_inputs.delete_one({'_id': entity['next_page']})
+            entity = next_entity
 
 transactions = collection.find({"coinbase": False})
 for transaction in tqdm(transactions):
     count = aggregated_inputs.count_documents({"tx_hash": transaction["tx_hash"]})
     if count == 0:
-        entity = get_entity_from_inputs(transaction["inputs"])
-        entity_exist = aggregated_inputs.find_one({"_id": entity})
-        if entity_exist:
-            aggregated_inputs.update_one({
-                        '_id': entity_exist['_id']
-                    }, 
-                        {'$push': {'tx_hash': transaction["tx_hash"]}
-                    }, upsert=False)
-        else:
-            addresses = get_addresses_from_entity(entity)
-            pagesize = 100000
-            if len(addresses) > pagesize:
-                aggregated_input =  {"_id": entity, "tx_hash": [transaction["tx_hash"]], "addresses": addresses[:pagesize]}
-                aggregated_inputs.insert_one(aggregated_input)
-                page = entity
-                for i in range(0, len(addresses[pagesize:]), pagesize):
-                    id = hashlib.sha1(str(time()).encode('utf-8')).hexdigest()
-                    inserted_document = aggregated_inputs.insert_one({"_id": id, "addresses": addresses[i:pagesize]})
-                    aggregated_inputs.update_one(
-                        {'_id': page}, 
-                        {
-                            '$set': 
-                                {"next_page": inserted_document.inserted_id }
+        try:
+            entity = get_entity_from_inputs(transaction["inputs"])
+            entity_exist = aggregated_inputs.find_one({"_id": entity})
+            if entity_exist:
+                aggregated_inputs.update_one({
+                            '_id': entity_exist['_id']
+                        }, 
+                            {'$push': {'tx_hash': transaction["tx_hash"]}
                         }, upsert=False)
-                    page = id
             else:
-                aggregated_input = {"_id": entity, "tx_hash": [transaction["tx_hash"]], "addresses": addresses}
-                aggregated_inputs.insert_one(aggregated_input)
+                addresses = get_addresses_from_entity(entity)
+                pagesize = 100000
+                if len(addresses) > pagesize:
+                    aggregated_input =  {"_id": entity, "tx_hash": [transaction["tx_hash"]], "addresses": addresses[:pagesize]}
+                    aggregated_inputs.insert_one(aggregated_input)
+                    page = entity
+                    for i in range(0, len(addresses[pagesize:]), pagesize):
+                        id = hashlib.sha1(str(time()).encode('utf-8')).hexdigest()
+                        inserted_document = aggregated_inputs.insert_one({"_id": id, "addresses": addresses[i:i+pagesize]})
+                        aggregated_inputs.update_one(
+                            {'_id': page}, 
+                            {
+                                '$set': 
+                                    {"next_page": inserted_document.inserted_id }
+                            }, upsert=False)
+                        page = id
+                else:
+                    aggregated_input = {"_id": entity, "tx_hash": [transaction["tx_hash"]], "addresses": addresses}
+                    aggregated_inputs.insert_one(aggregated_input)
+        except graphsense.ApiException as e:
+            print("Exception:",
+                e.status, e.reason)
+            existing_entity = aggregated_inputs.find_one({"tx_hash": transaction["tx_hash"]})
+            if existing_entity:
+                delete_entity(existing_entity, transaction["tx_hash"])
+            sleep(10)
+            continue
--- getTransactionsTimeInterval.py
+++ getTransactionsTimeInterval.py
@@ -1,20 +1,28 @@
-import requests
 import os
 import graphsense
 from dotenv import load_dotenv
-from graphsense.api import blocks_api, general_api
+from graphsense.api import blocks_api, general_api, bulk_api
 import datetime
 from tqdm import tqdm
+from time import sleep
+import pymongo
 
 load_dotenv('.env')
 
 api_key = os.environ.get("api_key")
 configuration = graphsense.Configuration(host="https://api.graphsense.info")
 configuration.api_key["api_key"] = api_key
+connectURI = os.environ["connectURI"]
+client = pymongo.MongoClient(connectURI)
+db = client["master"]
+time_transactions = db["time-transactions"]
 
 api_client = graphsense.ApiClient(configuration)
+
 blocks_api = blocks_api.BlocksApi(api_client)
 general_api = general_api.GeneralApi(api_client)
+bulk_api = bulk_api.BulkApi(api_client)
+
 
 
 def find_block_by_timestamp_binary_search(low, high, timestamp, roundUp):
@@ -41,6 +49,57 @@ def get_block_timestamp(height):
     except graphsense.ApiException as e:
         print("Exception when calling BlocksApi->get_block: %s\n" % e)
 
+def get_io(tx_hashes, io):
+    io_list = []
+    i = 0
+    while i < len(tx_hashes):
+        try:
+            body = {
+                "tx_hash": tx_hashes[i:i+50], "io": io}
+            io_list.extend(bulk_api.bulk_json(
+                'btc', 'get_tx_io', 1, body, async_req=True).get())
+        except graphsense.ApiException as e:
+            if (e.status == 429):
+                sleep(int(e.headers["Retry-After"]) + 60)
+                continue
+            else:
+                raise e
+        sleep(3)
+        i += 50
+    return io_list
+
+def formatInputOutput(puts):
+    formatted_puts = []
+    for put in puts:
+        if "_info" in put and put["_info"] == "no data":
+            continue
+        address = []
+        try:
+            address = [put["address"][0][""]]
+        except IndexError:
+            pass
+        formatted_puts.append({"_request_tx_hash": put["_request_tx_hash"], "address": address, "value": {"fiat_values" : [{"code": "eur", "value": put["value_eur"]}, {"code": "usd", "value": put["value_usd"]}], "value": int(put["value_value"])}})
+    return formatted_puts
+
+def formatValue(value):
+    return {"value": int(value["value"]), "fiat_values": [{"code": fv["code"], "value": fv["value"]} for fv in value["fiat_values"]]}
+    
+
+
+def format_transactions(transactions, inputs, outputs):
+    formatted_transactions = []
+    inputs = formatInputOutput(inputs)
+    outputs = formatInputOutput(outputs)
+    for transaction in transactions:
+        fortmatted_transaction = {"_id": transaction["tx_hash"], "coinbase": transaction["coinbase"], "height": None, "inputs": [], "outputs": [], "timestamp": transaction["timestamp"], "total_input": {}, "total_output": {}, "tx_hash": transaction["tx_hash"], "tx_type": transaction["tx_type"]}
+        fortmatted_transaction["height"] = transaction["height"]["value"]
+        fortmatted_transaction["total_input"] = formatValue(transaction["total_input"])
+        fortmatted_transaction["total_output"] = formatValue(transaction["total_output"])
+        fortmatted_transaction["inputs"] = [{i:inp[i] for i in inp if i!='_request_tx_hash'} for inp in inputs if inp["_request_tx_hash"] == transaction["tx_hash"]]
+        fortmatted_transaction["outputs"] = [{i:output[i] for i in output if i!='_request_tx_hash'} for output in outputs if output["_request_tx_hash"] == transaction["tx_hash"]]
+        formatted_transactions.append(fortmatted_transaction)
+    return formatted_transactions
+
 
 def get_transactions_in_time_interval(start_date, end_date):
     graphsense_statistics = general_api.get_statistics()
@@ -51,15 +110,23 @@ def get_transactions_in_time_interval(start_date, end_date):
         0, latest_block_height, start_date, True)
     end_block = find_block_by_timestamp_binary_search(
         start_block, latest_block_height, end_date, False)
+    return (start_block, end_block)
 
-    transactions = []
-    for block_height in tqdm(range(start_block, end_block+1)):
-        block_transactions = blocks_api.list_block_txs('btc', block_height)
-        transactions.extend(block_transactions)
-    return transactions
+start_date = datetime.datetime(2017, 12, 18).timestamp()
+end_date = datetime.datetime(2017, 12, 18).timestamp() + 3600 * 24
 
-
-start_date = datetime.datetime(2022, 2, 28).timestamp()
-end_date = datetime.datetime(2022, 2, 28).timestamp() + 3600
-
-print(len(get_transactions_in_time_interval(start_date, end_date)))
+start_block, end_block = get_transactions_in_time_interval(start_date, end_date)
+for block_height in tqdm(range(start_block, end_block+1)):
+    count = time_transactions.count_documents({"height": block_height})
+    if count == 0:
+        try:
+                block_transactions = blocks_api.list_block_txs('btc', block_height)
+                tx_hashes = [transaction["tx_hash"] for transaction in block_transactions]
+                inputs = get_io(tx_hashes, "inputs")
+                outputs = get_io(tx_hashes, "outputs")
+                time_transactions.insert_many(format_transactions(block_transactions, inputs, outputs))
+        except graphsense.ApiException as e:
+            print("Exception:",
+                e.status, e.reason)
+            continue
+        sleep(10)
